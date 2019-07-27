7071b6ea8bca35e5c021f26c387dfd28e8d6c80c
t-tajiri <swallow.ava@gmail.com>
2019-07-28 02:12:49 +0900

    change title in my blog

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -9,7 +9,7 @@ const config: NuxtConfiguration = {
   ** Headers of the page
   */
   head: {
-    title: process.env.npm_package_name || '',
+    title: 'my blog',
     meta: [
       { charset: 'utf-8' },
       { name: 'viewport', content: 'width=device-width, initial-scale=1' },
