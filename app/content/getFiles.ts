e4f46066b945dd5955c58c528d87b88054694336
t-tajiri <swallow.ava@gmail.com>
2020-04-11 15:08:18 +0900

    refactor: separate of concerns

---
+++ app/content/getFiles.ts
@@ -0,0 +1,24 @@
+import * as fs from 'fs'
+import { BlogContent } from '~/app/content/index2'
+const parseMarkdown = require('front-matter-markdown')
+
+export function getFiles(dir) {
+  let files: Array<BlogContent> = []
+
+  // synchronous i/o function is better at performance
+  const f = fs.readdirSync(dir)
+
+  f.forEach((file) => {
+    const markdownFile = fs.readFileSync(`app/content/blog/${file}`, 'utf-8')
+    const fileContents = parseMarkdown(markdownFile)
+
+    const date = fileContents.date
+    const slug = file.split('.')[0].toString()
+
+    const obj = { date, slug }
+
+    files.push(obj)
+  })
+
+  return files
+}
